91940925ca49873ad2ce3cd421a92cdb525b1597
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2017-04-19 18:18:16 -0500

    feat: initial commit, only header

---
+++ components/footer.js
@@ -0,0 +1,5 @@
+export default () => (
+<footer>
+    <p>footer</p>
+</footer>
+)
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Page from '../layouts/main'
+
+export default () => (
+  <Page>
+    <p>my page with global styles!</p>
+  </Page>
+)
