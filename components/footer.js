9b4ceb1d3cf5c944cb87ca7fa6f2eda38cc5419e
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2017-04-20 15:06:34 -0500

    feat(footer): init footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,45 @@
 export default () => (
 <footer>
-    <p>footer</p>
+    <div className="container">
+        <div className="row">
+            <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <ul className="list-inline text-center">
+                    <li>
+                        <a href="https://facebook.com/academyForUs" target="_blank">
+                            <span className="fa-stack fa-lg">
+                                    <i className="fa fa-circle fa-stack-2x"></i>
+                                    <i className="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.youtube.com/channel/UC8W7MstX6lCJMmX1Q9WuUWA" target="_blank">
+                            <span className="fa-stack fa-lg">
+                                    <i className="fa fa-circle fa-stack-2x"></i>
+                                    <i className="fa fa-youtube-play fa-stack-1x fa-inverse"></i>
+                                </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/ouracademy" target="_blank">
+                            <span className="fa-stack fa-lg">
+                                    <i className="fa fa-circle fa-stack-2x"></i>
+                                    <i className="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://medium.com/ouracademy" target="_blank">
+                            <span className="fa-stack fa-lg">
+                                    <i className="fa fa-circle fa-stack-2x"></i>
+                                    <i className="fa fa-medium fa-stack-1x fa-inverse"></i>
+                                </span>
+                        </a>
+                    </li>
+                </ul>
+                <p className="copyright text-muted">Copyright &copy; Ouracademy { new Date().getFullYear() }</p>
+            </div>
+        </div>
+    </div>
 </footer>
 )
--- components/posts.js
+++ components/posts.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 export default ({posts}) => (
-    <div classNameName="container">
+    <div className="container">
         <div className="row">
             <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 {
