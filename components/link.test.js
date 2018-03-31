3020da9912874591894c4a2430906fea6eddd578
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2018-03-31 15:32:28 +0000

    test: setup & test <Link/>

---
+++ components/link.test.js
@@ -0,0 +1,11 @@
+import { shallow } from 'enzyme'
+import renderer from 'react-test-renderer'
+
+import Link from '../components/link'
+
+describe('Link', () => {
+  it('should render children', () => {
+    const app = shallow(<Link to="someURI">content</Link>)
+    expect(app.equals(<a href="someURI" target="__blank">content</a>)).toBeTruthy()
+  })
+})
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/.c9/'
+  ]
+}
