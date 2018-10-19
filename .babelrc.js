69c8c868fbab9078950affb114ca41c145a7989d
artmadeit <this.arthur.email@gmail.com>
2018-10-19 07:57:25 +0000

    chore: upgrade dependencies

--- .babelrc.js
+++ .babelrc.js
@@ -1,21 +1,23 @@
-{
+const env = require('./env-config')
+
+module.exports = {
   "env": {
     "development": {
       "presets": ["next/babel"],
       "plugins": [
-        ["transform-define", "./env-config.js"]
+        ["transform-define", env]
       ]
     },
     "production": {
       "presets": ["next/babel"],
       "plugins": [
-        ["transform-define", "./env-config.js"]
+        ["transform-define", env]
       ]
     },
     "test": {
       "presets": [["next/babel", { "preset-env": { "modules": "commonjs" } }]],
        "plugins": [
-        ["transform-define", "./env-config.js"]
+        ["transform-define", env]
       ]
     }
   }
