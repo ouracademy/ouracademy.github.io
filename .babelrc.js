440377e91ff2e68ad06bb9ba0a5a6a17e76c934a
artmadeit <this.arthur.email@gmail.com>
2018-10-19 18:04:13 +0000

    chore: move to babel 7

--- .babelrc.js
+++ .babelrc.js
@@ -1,18 +1,6 @@
 const env = require('./env-config')
 
 module.exports = {
-  env: {
-    development: {
-      presets: ['next/babel'],
-      plugins: [['transform-define', env]]
-    },
-    production: {
-      presets: ['next/babel'],
-      plugins: [['transform-define', env]]
-    },
-    test: {
-      presets: [['next/babel', { 'preset-env': { modules: 'commonjs' } }]],
-      plugins: [['transform-define', env]]
-    }
-  }
+  presets: [['@babel/preset-env', {modules: 'commonjs'}], 'next/babel'],
+  plugins: [['transform-define', env]]
 }
