9a20cb1224aea2e7fcaa7d7aa6c520d431c712dd
artmadeit <this.arthur.email@gmail.com>
2018-10-22 03:11:35 +0000

    style: format extract-post.spec

--- extract-post.spec.js
+++ extract-post.spec.js
@@ -1,9 +1,8 @@
 const extractPost = require('./extract-post')
 
 describe('extractPost', () => {
-    it('should extract the props of <Post> as an post object', () => {
-        const snippet = 
-        `import Post from './components/post'
+  it('should extract the props of <Post> as an post object', () => {
+    const snippet = `import Post from './components/post'
 
         
         export default () => (
@@ -17,14 +16,13 @@ describe('extractPost', () => {
               Some content
             </Post>
           )`
-        
-        expect(extractPost(snippet)).toEqual({
-            title: 'Lo primero',
-            description: '¿Por que las pruebas de software son lo primero?',
-            publishedAt: "2018-02-21",
-            author:"@arthur",
-            tags: ['practica', 'testing']
-        })
-        
+
+    expect(extractPost(snippet)).toEqual({
+      title: 'Lo primero',
+      description: '¿Por que las pruebas de software son lo primero?',
+      publishedAt: '2018-02-21',
+      author: '@arthur',
+      tags: ['practica', 'testing']
     })
+  })
 })
