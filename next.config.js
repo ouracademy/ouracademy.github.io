7a6c05f1770e92e0ab5783c785a8c3d0d31d3449
artmadeit <this.arthur.email@gmail.com>
2018-10-23 04:19:40 +0000

    style: format next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 const pickBy = require('lodash/fp/pickBy')
 
 module.exports = {
-  exportPathMap: async function (defaultPathMap) {
-
-    return pickBy(
-        x => { 
-            return !x.page.endsWith('.test')
-        }
-    )(defaultPathMap)
+  exportPathMap: async function(defaultPathMap) {
+    return pickBy(x => {
+      return !x.page.endsWith('.test')
+    })(defaultPathMap)
   }
 }
