d28c1098ddc24f07b74442a9631daa081d994895
artmadeit <this.arthur.email@gmail.com>
2018-10-24 00:09:38 -0500

    chore: update about description

--- pages/about.js
+++ pages/about.js
@@ -20,8 +20,8 @@ export default () => {
           <section title="nosotros" className="row">
             <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
               <p>
-                Somos un grupo de amigos que queremos aprender y enseñar buenas
-                practicas,tecnologias y teorias que involucran desarrollar
+                Somos un grupo de amigos que se divierte aprendiendo y enseñando
+                practicas, principios tecnologías y teorías en el desarrollo
                 software.
               </p>
             </div>
